6da45ea7aee2f257ac7bfc41c3d058bc796ba20b
akumargs <mail.arvind85@gmail.com>
2019-05-20 15:32:13 +0530

    Regression due to which only 'events.txt' is always read, instead of the argument passed.

--- unbabel_cli.py
+++ unbabel_cli.py
@@ -5,8 +5,8 @@ from lib.ComputeTimeSeries import computeTimeSeries
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Computes and returns a ordered time series of average delivery time and other statistics from given input file, in a specific window/time-frame")
-    parser.add_argument('--input-file', default=open('events.txt', 'r'), dest='eventsFile', metavar="INPUT-FILE",
-                        type=argparse.FileType('r'),
+    parser.add_argument('--input-file', default='events.txt', dest='eventsFile', metavar="INPUT-FILE",
+                        type=str,
                         required=True,
                         help='(Required) File containing discrete events seperated by new line.')
     parser.add_argument('--window-size', dest='timeFrame', metavar="N",
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     parser.add_argument('--time-slot', default=1, dest='timeSlot', metavar="T", type=int,
                         help="(Optional) Time slot duration in minutes. Defaults to 1 min")
     args = parser.parse_args()
-    timeUnits = computeTimeSeries(args.timeFrame, FileHandler.read(args.eventsFile), args.timeSlot)
+    timeUnits = computeTimeSeries(args.timeFrame, FileHandler.read(open(args.eventsFile)), args.timeSlot)
     FileHandler.write('output.txt', timeUnits)
